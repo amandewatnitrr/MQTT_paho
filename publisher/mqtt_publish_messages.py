042eb05314417e48ad8547ae124b3b8db42a12df
Aman Kumar Dewangan <amandewatnitrr@gmail.com>
2021-06-25 13:52:20 +0530

    Update mqtt_publish_messages.py

--- publisher/mqtt_publish_messages.py
+++ publisher/mqtt_publish_messages.py
@@ -1,5 +1,11 @@
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import time
+import time
+import requests
+import random
+import decimal
+import csv
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
@@ -11,8 +17,14 @@ client.connect("broker.emqx.io", 1883, 60)
 # send a message to the raspberry/topic every 1 second, 5 times in a row
 for i in range(5):
     # the four parameters are topic, sending content, QoS and whether retaining the message respectively
-    client.publish('raspberry/topic', payload=i, qos=0, retain=False)
-    print(f"send {i} to raspberry/topic")
-    time.sleep(1)
+    with open('dataset.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            t = str(f'{row["Timestamp"]}')
+            k = str(f'{row["Value"]}')
+            s = str(f'{row["Sensor"]}')
+            data = t+","+k+","+s
+            client.publish('raspberry/topic', payload=data, qos=0, retain=False)
+            print(f"send {data} to raspberry/topic")
+            time.sleep(1)
 
-client.loop_forever()
